03aed07be188202099c77d86a16f0bb8098a9f75
Mike Vu <anhsaodem@yahoo.com>
2018-05-26 10:34:23 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,89 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+const express = require('express');
+const hbs = require('hbs');;
+const fs = require('fs')
+
+const currentYear  = new Date().getFullYear();
+
+const MAINTENANCE = false;
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+hbs.registerHelper('getCurrentYear', () => {return new Date().getFullYear()});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+
+
+
+app.use((req, res, next) => {
+	let now = new Date().toString();
+	let msg = `${now}: ${req.method}:${req.url}`;
+	
+	fs.appendFile('server.log', msg + "\n", (err) =>{});
+	
+	next();
+});
+
+app.use((req, res, next) => {
+	if(MAINTENANCE)
+		{
+			res.render('maintenance.hbs',{
+				pageTitle : 'Healthy Eating..',
+				homeTitle : 'Healthy Life',
+				maintWindow : '2-3 am'
+				
+			});
+		}
+	else
+		{
+			next();
+		}
+	
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/hobbies', (req, res) => {
+	res.send({
+		name: 'Mike Vu',
+		likes: [
+			'Biking',
+			'Cities'
+		]
+	});
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle : 'Healhy Eating',
+		currentYear : currentYear,
+		homeTitle : 'Healthy Eating habits...' 
+	})
+	
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+		currentYear : currentYear
+		
+	});
+})
+
+app.get('/info', (req, res) => {
+	res.send({
+		cat: "Health",
+		toptic: "How to cure psoriasis"
+	});
+});
+
+app.listen(3000);
